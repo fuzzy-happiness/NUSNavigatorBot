3e5c2ee62cb7aa7adf5c9012f9c491cd6f21d56e
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-28 02:17:41 +0800

    Implemented nearest location (#5)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const dotenv = require('dotenv');
 const Session = require('./lib/session');
 
+const ONE_DEGREE = 111319.9;
+
 dotenv.load();
 
 const token = process.env.TOKEN;
@@ -10,11 +12,29 @@ const bot = new TelegramBot(token, { polling: true });
 
 const session = new Session();
 
+function findNearest(loc, list) {
+  let min = { distance: Infinity };
+  for(var i = 0; i < list.length; i++) {
+    let distance = Math.sqrt(Math.pow(loc.latitude - list[i].latitude, 2) + Math.pow(loc.longitude - list[i].longitude, 2));
+    if (distance < min.distance) {
+      min = { distance: distance, index: i };
+    }
+  }
+  return min;
+}
+
+function processFromLocation(msg) {
+  session.set(msg.chat.id, 'from', { "latitude": msg.location.latitude, "longitude": msg.location.longitude });
+  const bus_stops = require('./bus_stops.json');
+  const nearest = findNearest(msg.location, bus_stops);
+  bot.sendMessage(msg.chat.id, `Nearest bus stop is ${bus_stops[nearest.index].name}, approximately ${Math.round(nearest.distance * ONE_DEGREE)} metre(s)`);
+}
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   if (Object.prototype.hasOwnProperty.call(msg, 'location')) {
-    processFromLocation(msg);
+    return processFromLocation(msg);
   }
 
   switch (msg.text) {
@@ -24,9 +44,12 @@ bot.on('message', (msg) => {
     case '/about':
       bot.sendMessage(chatId, 'This bot is created by @indocomsoft and @sciffany during Hack & Roll 2018!');
       break;
+    case '/to':
+      break;
     default:
       bot.sendMessage(chatId, 'Command not recognised');
       break;
   }
+  return true;
 });
 
