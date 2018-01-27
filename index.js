ace096689b34ed8758ec2e70745dc414c5d8664d
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-28 00:30:16 +0800

    Basic parser done

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ dotenv.load();
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  if (Object.prototype.hasOwnProperty.call(msg, 'location')) {
+    processFromLocation(msg);
+  }
+
+  switch (msg.text) {
+    case '/start':
+      bot.sendMessage(chatId, 'Welcome! Please send me your location so I can guide you!');
+      break;
+    case '/about':
+      bot.sendMessage(chatId, 'This bot is created by @indocomsoft and @sciffany during Hack & Roll 2018!');
+      break;
+    default:
+      bot.sendMessage(chatId, 'Command not recognised');
+      break;
+  }
+});
+
