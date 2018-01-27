53a347de300ac95856ac4a81699909821df210ce
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-27 23:54:53 +0800

    Added basic parts

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
+
+dotenv.load();
+
 const token = process.env.TOKEN;
 
+const bot = new TelegramBot(token, { polling: true });
