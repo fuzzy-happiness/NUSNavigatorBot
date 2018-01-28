1154aa63ddf7ca7f3956dec0176775fa8e50839e
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-28 09:28:18 +0800

    Added parser and interface for from and to (#6)

--- index.js
+++ index.js
@@ -24,32 +24,98 @@ function findNearest(loc, list) {
 }
 
 function processFromLocation(msg) {
-  session.set(msg.chat.id, 'from', { "latitude": msg.location.latitude, "longitude": msg.location.longitude });
   const bus_stops = require('./bus_stops.json');
   const nearest = findNearest(msg.location, bus_stops);
   bot.sendMessage(msg.chat.id, `Nearest bus stop is ${bus_stops[nearest.index].name}, approximately ${Math.round(nearest.distance * ONE_DEGREE)} metre(s)`);
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ['UTown'],
+        ['SoC', 'BIZ', 'FASS', 'FoE/SDE'],
+        ['FoS', 'YLLSM', 'YSTCM'],
+        ['YIH', 'CLB/COMCEN', 'Halls'],
+        ['PGPR', 'Kent Ridge MRT', 'Misc']
+      ],
+      resize_keyboard: true
+    })
+  };
+  bot.sendMessage(msg.chat.id, `Which faculty are you going to?`, opts);
+  session.set(msg.chat.id, 'from', bus_stops[nearest.index]);
+  return session.set(msg.chat.id, 'to', 1);
+}
+
+function rowify(list, columnWidth) {
+  let result = [];
+  for (let i = 0; i < Math.ceil(list.length / columnWidth); i++) {
+    result.push(list.slice(i * columnWidth, (i + 1) * columnWidth));
+  }
+  return result;
+}
+
+function route(msg, from, to) {
+  const opts = {
+    reply_markup: JSON.stringify({
+      remove_keyboard: true
+    })
+  };
+  let directions = `You are going from ${from.name} to ${to.name}`;
+  bot.sendMessage(msg.chat.id, directions, opts);
+}
+
+function processToLocation(msg) {
+  const from = session.get(msg.chat.id, 'from');
+  const bus_stops = require('./bus_stops.json');
+  if (session.get(msg.chat.id, 'to') === 1) {
+    const filtered = bus_stops.filter(obj => obj.group === msg.text);
+    if (filtered.length === 0) {
+      bot.sendMessage(msg.chat.id, 'Please use the selections below.');
+    } else if (filtered.length === 1) {
+      route(msg, from, filtered[0]);
+    } else {
+      const opts = {
+        reply_markup: JSON.stringify({
+          keyboard: rowify(filtered.map(obj => obj.name), 3),
+          resize_keyboard: true
+        })
+      }
+      bot.sendMessage(msg.chat.id, `Choose one of the ${msg.text} bus stops`, opts);
+    }
+    session.set(msg.chat.id, 'to', msg.text);
+  } else {
+    const filtered = bus_stops.filter(obj => obj.name === msg.text);
+    if (filtered.length === 1) {
+      route(msg, from, filtered[0]);
+    } else {
+      bot.sendMessage(msg.chat.id, `Please use the selections below.`);
+    }
+  }
 }
 
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+  try {
+    if (Object.prototype.hasOwnProperty.call(msg, 'location')) {
+      return processFromLocation(msg);
+    }
 
-  if (Object.prototype.hasOwnProperty.call(msg, 'location')) {
-    return processFromLocation(msg);
+    if (session.get(msg.chat.id, 'from') !== undefined ) {
+      return processToLocation(msg);
+    }
+
+    switch (msg.text) {
+      case '/start':
+        bot.sendMessage(msg.chat.id, 'Welcome! Please send me your location so I can guide you!');
+        break;
+      case '/about':
+        bot.sendMessage(msg.chat.id, 'This bot is created by @indocomsoft and @sciffany during Hack & Roll 2018!');
+        break;
+      default:
+        bot.sendMessage(msg.chat.id, 'Command not recognised');
+        break;
+    }
+    return true;
+  } catch (e) {
+    console.log(e);
   }
 
-  switch (msg.text) {
-    case '/start':
-      bot.sendMessage(chatId, 'Welcome! Please send me your location so I can guide you!');
-      break;
-    case '/about':
-      bot.sendMessage(chatId, 'This bot is created by @indocomsoft and @sciffany during Hack & Roll 2018!');
-      break;
-    case '/to':
-      break;
-    default:
-      bot.sendMessage(chatId, 'Command not recognised');
-      break;
-  }
-  return true;
 });
 
